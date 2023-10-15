e551dafe2910ab844869eac500d8c4fd6ac8faf2
Bartosz <bartoszweb8@gmail.com>
2023-10-15 12:15:19 +0200

    changed host and url in server.js idk

--- config/server.js
+++ config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
-  host: env('STRAPI_HOST', 'api.genialnastrona.pl'), 
-  port: env.int('STRAPI_PORT', 1337),
-  url: env('STRAPI_URL', 'https://api.genialnastrona.pl'), 
-  app: {
+   host: env('STRAPI_HOST', 'api.genialnastrona.pl'), 
+   port: env.int('STRAPI_PORT', 1337),
+   url: env('STRAPI_URL', 'https://api.genialnastrona.pl'), 
+   app: {
     keys: env.array('APP_KEYS'),
   },
   webhooks: {
